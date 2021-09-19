dfb636859e2ece98d344c0e6bded9bd20225a0f9
Scott Coates <scoarescoare@gmail.com>
2021-09-19 16:13:08 -0500

    making heroku ready

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
-username = "RedditUsername"
-password = "password"
-client_id = "idGoesHere"
-client_secret = "secretGoesHere"
+import os
+
+try:
+    # https://github.com/theskumar/python-dotenv
+    # todo make multi env https://stackoverflow.com/questions/17803829/how-to-customize-a-requirements-txt-for-multiple-environments
+    from dotenv import load_dotenv
+    load_dotenv()  # take environment variables from .env.
+    # Code of your application, which uses environment variables (e.g. from `os.environ` or
+    # `os.getenv`) as if they came from the actual environment.
+except:
+    pass
+
+subreddit = os.environ['SUBREDDIT']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
